3149c7ef664ad69659f4bd7919e1aabbd3747050
sonic912 <vel-si@yandex.ru>
2019-08-17 21:01:58 +0300

    11_lesson-modal

---
+++ 11lesson.js
@@ -0,0 +1,6 @@
+
+let age = document.getElementById('age');
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+showUser.apply(age, ['Name', 'Surname']);
--- js/script.js
+++ js/script.js
@@ -80,6 +80,33 @@ window.addEventListener('DOMContentLoaded', function() {
              }
         }    
     }
+    setClock('timer', deadLine);
 
-    setClock('timer', deadLine)
+    //modal
+    let more = document.querySelector('.more'),
+        sliderBtn = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        showModal();
+    });
+    
+    sliderBtn.forEach(function(event) {
+        event.addEventListener('click', function() {
+            showModal();
+        });
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        this.classList.remove('more-splash');
+        document.body.style.overflow = ''; 
+    });
+
+    function showModal() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    };
 });
