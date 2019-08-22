3df3df8cd5c43b802a8f0fd90e6afe9f4aa223f4
sonic912 <vel-si@yandex.ru>
2019-08-22 12:48:29 +0300

    14.2_lesson-promise

--- cur_calc/js/script.js
+++ cur_calc/js/script.js
@@ -1,33 +1,30 @@
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-
-function calcCurrency(elem) {
-    elem.addEventListener('input', function() {
+inputRub.addEventListener('input', () => {    
         function getCurrValue() {
             return new Promise(function(resolve, reject) {
                 let req = new XMLHttpRequest();
                 req.open('GET', 'js/current.json');
                 req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-                req.onreadystatechange = function() {
+                req.send();
+                req.onload = function() {
                     if(req.readyState === 4 && req.status == 200) {
-                        resolve();
+                        resolve(this.response);
                     } else {
                         reject();
                     }    
                 };
-                req.send();
+                
                 // let data = JSON.parse(req.response);
             });
         }
         
         getCurrValue()
-                    .then(() => inputUsd.value = inputRub.value/(JSON.parse(req.response).usd))
+                    .then(response => inputUsd.value = inputRub.value/(JSON.parse(response).usd))
                     .catch(() => inputUsd.value = "Err!")
     });
-}
 
-calcCurrency(inputRub);
 
 
 
